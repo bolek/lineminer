37e928a3dc70ce22cc3dcf831911694d4b2ca071
Bolek Kurowski <bolek@alumni.cmu.edu>
2014-09-17 00:52:52 -0400

    add run.sh

---
+++ run.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+if [ $# -eq 0 ]
+    then
+    echo "No file provided. Please provide a path to the source file."
+    exit 0
+fi
+
+if [ ! -f $1 ];
+    then
+        echo "Invalid path to file."
+        exit 0
+fi
+
+echo $1
+cp $1 lineminer-sinatra/assets/datafile
+
+cd lineminer-sinatra
+ruby app.rb
