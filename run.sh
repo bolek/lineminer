270198d575f70aae02a0b06a6818517ddd167382
Bolek Kurowski <bolek@alumni.cmu.edu>
2014-09-20 16:02:31 -0400

    generate config.yml when starting server

--- run.sh
+++ run.sh
@@ -11,9 +11,20 @@ if [ ! -f $1 ];
         exit 0
 fi
 
-echo $1
-cp $1 lineminer-sinatra/assets/datafile
+realpath_osx() {
+    [[ $1 = /* ]] && echo "$1" || echo "$PWD/${1#./}"
+}
+
+ABSOLUTE_PATH=$(realpath_osx "$1")
+
+echo 'Generating config'
+cp lineminer-sinatra/config_tpl.yml lineminer-sinatra/config.yml
+sed -i '' "s?#{datafile_production}?${ABSOLUTE_PATH}?g" lineminer-sinatra/config.yml
 
 cd lineminer-sinatra
+
+echo 'Setting environment variables'
 export RACK_ENV='production'
+
+echo 'Initiating server'
 ruby app.rb -s Puma
